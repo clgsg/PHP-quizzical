77a3a53546e846f9dd52d3af45805bc7a9263748
clgsg <clg_listas@yahoo.es>
2022-03-14 19:51:09 +0100

    wip: connection after correct signup

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+
+
+		// #Connect to db:
+		// $connect = mysqli_connect("localhost","root","","examen");
+
+		// if (mysqli_connect_errno())
+		// {
+		// echo "Connection failed: " . mysqli_connect_error();
+		// }
+		// else
+		// {
+		// echo "Connection established 🚀.<br>";
+		// }
+
+	?>
